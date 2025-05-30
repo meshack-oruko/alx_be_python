79b10da067fe0511ad240cd65315854b9c4018dd
Meshack Otieno <meshackb59@gmail.com>
2025-05-30 11:55:59 +0300

    calculator updateed

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,18 +5,18 @@ operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
         result = float(num1) + float(num2)
-        print(f"The result of {num1} + {num2} is: {result}")
+        print(f"The result is: {result}")
     case "-":
         result = float(num1) - float(num2)
-        print(f"The result of {num1} - {num2} is: {result}")
+        print(f"The result of is: {result}")
     case "*":
         result = float(num1) * float(num2)
-        print(f"The result of {num1} * {num2} is: {result}")
+        print(f"The result of is: {result}")
     case "/":
         if float(num2) == 0:
             print("Cannot devide with zero.")
         else:
             result = float(num1) / float(num2)
-            print(f"The result of {num1} / {num2} is: {result}")
+            print(f"The result is: {result}")
     case _:
         print("Invalid operation. Please enter one of +, -, *, or /.")
