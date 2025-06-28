61df5c1c9e1aac8a526717ea93e6d77be88be7a3
Meshack Otieno <meshackb59@gmail.com>
2025-06-28 18:31:38 +0300

    my testfile

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,4 +18,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.__account_balance:.2f}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
+
